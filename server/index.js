cdcedb0cc34245a1dabec6844f339d0126fca11c
Don Heshanthaka <heshanthaka007@gmail.com>
2023-02-20 11:46:29 +0530

    Implement mongoose setup

--- server/index.js
+++ server/index.js
@@ -23,5 +23,15 @@ app.use(cors())
 app.use("/assets", express.static(path.join(__dirname, "public/assets")))
 
 
+// Mongoose setup
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+
+  })
+  .catch((error) => console.log(`${error} did not connect`))

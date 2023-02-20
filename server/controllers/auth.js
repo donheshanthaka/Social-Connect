2875e225694c4f8d764fb22f52ebe34da5c2cf2d
Don Heshanthaka <heshanthaka007@gmail.com>
2023-02-20 22:28:08 +0530

    Implement user creation and authentication

---
+++ server/controllers/auth.js
@@ -0,0 +1,94 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+// Register user
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      imageURL,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath: imageURL,
+      friends,
+      location,
+      occupation,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Login user
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Google authentication 
+export const googleAuth = async (req, res) => {
+  try {
+    const { email } = req.body;
+    let user = await User.findOne({ email: email });
+
+    if (!user) {
+      const {
+        firstName,
+        lastName,
+        email,
+        picturePath,
+        password,
+      } = req.body;
+
+      const salt = await bcrypt.genSalt();
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password: passwordHash,
+        picturePath,
+      });
+  
+      const user = await newUser.save();
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.status(201).json({token, user});
+    } else {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.status(200).json({ token, user });
+    }
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};

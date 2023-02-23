daa8e26b327455a061b0cfd6c0606233d02fafc4
Don Heshanthaka <heshanthaka007@gmail.com>
2023-02-24 04:40:28 +0530

    Implement login page

---
+++ client/src/components/AuthenticationFrom.jsx
@@ -0,0 +1,360 @@
+import { useState } from "react"
+import {
+  Box,
+  Button,
+  TextField,
+  useMediaQuery,
+  Typography,
+  useTheme,
+  Divider,
+} from "@mui/material"
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined"
+import { Formik } from "formik"
+import * as yup from "yup"
+import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { setLogin } from "state/state"
+import Dropzone from "react-dropzone"
+import FlexBetween from "components/FlexBetween"
+import { GoogleLogin } from "@react-oauth/google"
+import jwt_dedcode from "jwt-decode"
+import { Height } from "@mui/icons-material"
+
+const registerSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+  location: yup.string().required("required"),
+  occupation: yup.string().required("required"),
+  picture: yup.string().required("required"),
+})
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+})
+
+const initialValuesRegister = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  location: "",
+  occupation: "",
+  picture: "",
+}
+
+const initialValuesLogin = {
+  email: "",
+  password: "",
+}
+
+const Form = () => {
+  const [pageType, setPageType] = useState("login")
+  const { palette } = useTheme()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const isNonMobile = useMediaQuery("(min-width:600px)")
+  const isLogin = pageType === "login"
+  const isRegister = pageType === "register"
+
+  const register = async (values, onSubmitProps) => {
+    // this allows us to send form info with image
+    const formData = new FormData()
+    for (let value in values) {
+      formData.append(value, values[value])
+    }
+    formData.append("picturePath", values.picture.name)
+    formData.append("type", "profileImage")
+
+    const savedUserResponse = await fetch(
+      "http://localhost:3001/auth/register",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+    const savedUser = await savedUserResponse.json()
+    onSubmitProps.resetForm()
+
+    if (savedUser) {
+      setPageType("login")
+    }
+  }
+
+  const login = async (values, onSubmitProps) => {
+    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    })
+    const loggedIn = await loggedInResponse.json()
+    onSubmitProps.resetForm()
+    if (loggedIn) {
+      dispatch(
+        setLogin({
+          user: loggedIn.user,
+          token: loggedIn.token,
+        })
+      )
+      navigate("/home")
+    }
+  }
+
+  const googleAuth = async (response) => {
+    const decoded = jwt_dedcode(response.credential)
+    const { family_name, given_name, email, picture, sub } = decoded
+    const userData = {
+      firstName: family_name,
+      lastName: given_name,
+      email: email,
+      password: sub,
+      picturePath: picture,
+    }
+
+    const userLogin = await fetch("http://localhost:3001/auth/google", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userData),
+    })
+    const loggedIn = await userLogin.json()
+    if (loggedIn) {
+      dispatch(
+        setLogin({
+          user: loggedIn.user,
+          token: loggedIn.token,
+        })
+      )
+      navigate("/home")
+    }
+  }
+
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    if (isLogin) await login(values, onSubmitProps)
+    if (isRegister) await register(values, onSubmitProps)
+  }
+
+  return (
+    <Formik
+      onSubmit={handleFormSubmit}
+      initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
+      validationSchema={isLogin ? loginSchema : registerSchema}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        setFieldValue,
+        resetForm,
+      }) => (
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            // background: "red",
+            width: "80%",
+            display: "flex",
+            flexDirection: "column",
+            // alignItems: "center",
+            // justifyContent: "center"
+            height: "50%",
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap="5px"
+            height="50%"
+            // gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+            // sx={{
+            //   "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+            // }}
+          >
+            {isRegister && (
+              <>
+                <TextField
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.firstName}
+                  name="firstName"
+                  error={
+                    Boolean(touched.firstName) && Boolean(errors.firstName)
+                  }
+                  helperText={touched.firstName && errors.firstName}
+                  sx={{ gridColumn: "span 2", height: "10%" }}
+                />
+                <TextField
+                  label="Last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.lastName}
+                  name="lastName"
+                  error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                  helperText={touched.lastName && errors.lastName}
+                  sx={{ gridColumn: "span 2", height: "10%" }}
+                />
+                <TextField
+                  label="Location"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.location}
+                  name="location"
+                  error={Boolean(touched.location) && Boolean(errors.location)}
+                  helperText={touched.location && errors.location}
+                  sx={{ gridColumn: "span 4", height: "10%" }}
+                />
+                <TextField
+                  label="Occupation"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.occupation}
+                  name="occupation"
+                  error={
+                    Boolean(touched.occupation) && Boolean(errors.occupation)
+                  }
+                  helperText={touched.occupation && errors.occupation}
+                  sx={{ gridColumn: "span 4", height: "10%" }}
+                />
+                <Box
+                  gridColumn="span 4"
+                  border={`1px solid ${palette.neutral.medium}`}
+                  borderRadius="5px"
+                  p="1rem"
+                  // height="30px"
+                >
+                  <Dropzone
+                    acceptedFiles=".jpg,.jpeg,.png"
+                    multiple={false}
+                    onDrop={(acceptedFiles) =>
+                      setFieldValue("picture", acceptedFiles[0])
+                    }
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <Box
+                        {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                        // p="1rem"
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                        // height="30px"
+                      >
+                        <input {...getInputProps()} />
+                        {!values.picture ? (
+                          <p style={{ margin: "none" }}>Add Picture Here</p>
+                        ) : (
+                          <FlexBetween>
+                            <Typography>{values.picture.name}</Typography>
+                            <EditOutlinedIcon />
+                          </FlexBetween>
+                        )}
+                      </Box>
+                    )}
+                  </Dropzone>
+                </Box>
+              </>
+            )}
+
+            {!isRegister && (
+              <Box
+                // width="500px"
+                // backgroundColor="red"
+                // marginLeft="2rem"
+                // position="relative"
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                textAlign="center"
+                gap="10px"
+              >
+                <Divider color="black">
+                  <Typography fontSize="1.2rem">
+                    Continue with Google
+                  </Typography>
+                </Divider>
+                <Box
+                  display="flex"
+                  justifyContent="center"
+                  textAlign="center"
+                  // width="20%"
+                  margin="0.5rem 0"
+                >
+                  <GoogleLogin
+                    onSuccess={(response) => {
+                      googleAuth(response)
+                    }}
+                    onError={() => console.log("error")}
+                  />
+                </Box>
+                <Divider color="black" sx={{ mb: "0.5rem" }}>
+                  <Typography fontSize="1.2rem">Standard Login</Typography>
+                </Divider>
+              </Box>
+            )}
+
+            <TextField
+              label="Email"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+              name="email"
+              error={Boolean(touched.email) && Boolean(errors.email)}
+              helperText={touched.email && errors.email}
+              sx={{ gridColumn: "span 4", height: "10%" }}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              name="password"
+              error={Boolean(touched.password) && Boolean(errors.password)}
+              helperText={touched.password && errors.password}
+              sx={{ gridColumn: "span 4", height: "10%" }}
+            />
+          </Box>
+
+          {/* BUTTONS */}
+          <Box>
+            <Button
+              fullWidth
+              type="submit"
+              sx={{
+                m: "2rem 0",
+                p: "1rem",
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                "&:hover": { color: palette.primary.main },
+              }}
+            >
+              {isLogin ? "LOGIN" : "REGISTER"}
+            </Button>
+            <Typography
+              onClick={() => {
+                setPageType(isLogin ? "register" : "login")
+                resetForm()
+              }}
+              sx={{
+                textDecoration: "underline",
+                color: palette.neutral.dark,
+                "&:hover": {
+                  cursor: "pointer",
+                  color: palette.neutral.main,
+                },
+              }}
+            >
+              {isLogin
+                ? "Don't have an account? Sign Up here."
+                : "Already have an account? Login here."}
+            </Typography>
+          </Box>
+        </form>
+      )}
+    </Formik>
+  )
+}
+
+export default Form
--- client/src/scenes/Pages/LoginPage.jsx
+++ client/src/scenes/Pages/LoginPage.jsx
@@ -1,5 +1,77 @@
-const LoginPage = () => {
-  return <div>loginpage</div>
-};
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material"
+import { useSelector } from "react-redux"
+import GlassmorphicWrapper from "components/GlassmorphicWrapper"
+import Form from "../../components/AuthenticationForm"
 
-export default LoginPage;
+const LoginPage = () => {
+  const theme = useTheme()
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+  const mode = useSelector((state) => state.mode)
+
+  let backgroundColor =
+    mode === "dark"
+      ? "linear-gradient(90deg, #2A5470 0%, #4C4177 100%)"
+      : "linear-gradient(90deg, #8EC5FC 0%, #E0C3FC 100%)"
+
+  return (
+    <Box height="100vh">
+      <Box
+        width="100%"
+        height="8%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Social Connect
+        </Typography>
+      </Box>
+
+      <Box
+        display="flex"
+        justifyContent="space-evenly"
+        alignItems="center"
+        minHeight="92%"
+        flexDirection={isNonMobileScreens ? "row" : "column"}
+        sx={{ background: `${backgroundColor}` }}
+      >
+        {/* login start */}
+        <GlassmorphicWrapper
+          width={isNonMobileScreens ? "40%" : "80%"}
+          p="2rem"
+          mb="2rem"
+          marginTop={isNonMobileScreens ? "0" : "2rem"}
+          borderRadius="1.5rem"
+          minHeight={isNonMobileScreens ? "80vh" : "40vh"}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+        >
+          <Typography
+            fontWeight="500"
+            variant="h5"
+            fontSize={isNonMobileScreens ? "1.5rem" : "1.2rem"}
+            sx={{ marginBottom: "3rem", textAlign: "center" }}
+          >
+            Welcome to Social Connect, the Social Media platform of the Next
+            Generation!
+          </Typography>
+
+          <Box
+            display="flex"
+            flexDirection="column"
+            textAlign="center"
+            justifyContent="center"
+            alignItems="center"
+            height="80%"
+          >
+            <Form></Form>
+          </Box>
+        </GlassmorphicWrapper>
+        {/* login end */}
+      </Box>
+    </Box>
+  )
+}
+
+export default LoginPage

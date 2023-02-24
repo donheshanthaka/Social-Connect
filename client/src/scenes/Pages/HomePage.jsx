4095a010747181ac42bca4d31cd5f0ac271b6548
Don Heshanthaka <heshanthaka007@gmail.com>
2023-02-24 18:59:30 +0530

    Implement user widget

--- client/src/scenes/Pages/HomePage.jsx
+++ client/src/scenes/Pages/HomePage.jsx
@@ -1,10 +1,46 @@
+import { Box, useMediaQuery } from "@mui/material"
+import { useSelector } from "react-redux"
 import Navbar from "components/Navbar"
+import UserWidget from "scenes/widgets/UserWidget"
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)")
+  const { _id, picturePath } = useSelector((state) => state.user)
+  const mode = useSelector((state) => state.mode)
+
+  let backgroundColor =
+    mode === "dark"
+      ? "linear-gradient(90deg, #2A5470 0%, #4C4177 100%)"
+      : "linear-gradient(90deg, #8EC5FC 0%, #E0C3FC 100%)"
+
   return (
-    <div>
-      <Navbar></Navbar>
-    </div>
+    <Box sx={{ background: `${backgroundColor}` }} minHeight="100vh">
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <Box m="2rem 0" />
+          </Box>
+        )}
+
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        ></Box>
+
+        {isNonMobileScreens && (
+          <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+            <UserWidget userId={_id} picturePath={picturePath} />
+          </Box>
+        )}
+      </Box>
+    </Box>
   )
 }
 
---
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,153 @@
+import { LocationOnOutlined, WorkOutlineOutlined } from "@mui/icons-material"
+import CodeIcon from "@mui/icons-material/Code"
+import LocalPoliceIcon from "@mui/icons-material/LocalPolice"
+import VerifiedIcon from "@mui/icons-material/Verified"
+import { Box, Typography, Divider, useTheme } from "@mui/material"
+import ProfileImage from "components/ProfileImage"
+import FlexBetween from "components/FlexBetween"
+import GlassmorphicWrapper from "components/GlassmorphicWrapper"
+import { useSelector } from "react-redux"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+const UserWidget = ({ userId, picturePath }) => {
+  const [user, setUser] = useState(null)
+  const { palette } = useTheme()
+  const navigate = useNavigate()
+  const token = useSelector((state) => state.token)
+  const dark = palette.neutral.dark
+  const medium = palette.neutral.medium
+  const main = palette.neutral.main
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    const data = await response.json()
+    setUser(data)
+  }
+
+  useEffect(() => {
+    getUser()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!user) {
+    return null
+  }
+
+  const { firstName, lastName, location, occupation } = user
+
+  return (
+    <GlassmorphicWrapper>
+      {/* FIRST ROW */}
+      <FlexBetween
+        gap="0.5rem"
+        pb="1.1rem"
+        onClick={() => navigate(`/profile/${userId}`)}
+      >
+        <FlexBetween gap="1rem">
+          <ProfileImage image={picturePath} />
+          <Box>
+            <Typography
+              variant="h4"
+              color={dark}
+              fontWeight="500"
+              textAlign="center"
+              sx={{
+                "&:hover": {
+                  color: palette.primary.light,
+                  cursor: "pointer",
+                },
+              }}
+            >
+              {firstName} {lastName}
+            </Typography>
+          </Box>
+          <VerifiedIcon color="primary"></VerifiedIcon>
+        </FlexBetween>
+      </FlexBetween>
+
+      <Divider />
+
+      {/* SECOND ROW */}
+      <Box p="1rem 0">
+        <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+          <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+          <Typography color={main}>{location}</Typography>
+        </Box>
+        <Box display="flex" alignItems="center" gap="1rem">
+          <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
+          <Typography color={main}>{occupation}</Typography>
+        </Box>
+      </Box>
+
+      <Divider />
+
+      <Box p="1rem 0">
+        <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+          Groups
+        </Typography>
+
+        <FlexBetween gap="1rem" mb="0.5rem">
+          <FlexBetween gap="1rem">
+            <CodeIcon sx={{ color: main }}></CodeIcon>
+            <Box>
+              <Typography color={main} fontWeight="500">
+                Code Masters
+              </Typography>
+              <Typography color={medium}>Software Engineering</Typography>
+            </Box>
+          </FlexBetween>
+        </FlexBetween>
+
+        <FlexBetween gap="1rem">
+          <FlexBetween gap="1rem">
+            <LocalPoliceIcon sx={{ color: main }}></LocalPoliceIcon>
+            <Box>
+              <Typography color={main} fontWeight="500">
+                Cyber Gate
+              </Typography>
+              <Typography color={medium}>Cyber Security</Typography>
+            </Box>
+          </FlexBetween>
+        </FlexBetween>
+      </Box>
+
+      <Divider />
+
+      {/* FOURTH ROW */}
+      <Box p="1rem 0">
+        <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+          Social Profiles
+        </Typography>
+
+        <FlexBetween gap="1rem" mb="0.5rem">
+          <FlexBetween gap="1rem">
+            <img src="../assets/twitter.png" alt="twitter" />
+            <Box>
+              <Typography color={main} fontWeight="500">
+                Twitter
+              </Typography>
+              <Typography color={medium}>Social Network</Typography>
+            </Box>
+          </FlexBetween>
+        </FlexBetween>
+
+        <FlexBetween gap="1rem">
+          <FlexBetween gap="1rem">
+            <img src="../assets/linkedin.png" alt="linkedin" />
+            <Box>
+              <Typography color={main} fontWeight="500">
+                Linkedin
+              </Typography>
+              <Typography color={medium}>Network Platform</Typography>
+            </Box>
+          </FlexBetween>
+        </FlexBetween>
+      </Box>
+    </GlassmorphicWrapper>
+  )
+}
+
+export default UserWidget

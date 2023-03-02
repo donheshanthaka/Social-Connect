efb4b7acad863b3239a02b53823ca14962969608
Don Heshanthaka <heshanthaka007@gmail.com>
2023-03-03 02:57:18 +0530

    Bug fix on posts widget

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -9,7 +9,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token)
 
   const getPosts = async () => {
-    const response = await fetch(`"${process.env.REACT_APP_SERVER_URI}/posts`, {
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URI}/posts`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     })
